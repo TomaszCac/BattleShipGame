9fd2238a4b753e9eaf027fbb225abb8cf407cdbf
Tomasz Cac <tomaszandrzej1234@gmail.com>
2026-05-17 13:16:56 +0200

    Added Initial Controllers

---
+++ BattleShipGame/Controllers/SessionController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BattleShipGame.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SessionController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllSessions()
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetSession(int id)
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult CreateSession()
+        {
+            return Ok();
+        }
+        [HttpDelete]
+        public IActionResult DeleteSession(int id)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ BattleShipGame/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BattleShipGame.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpGet("{id}")]
+        public IActionResult GetUser(int id)
+        {
+            return Ok();
+        }
+        [HttpPost]
+        public IActionResult CreateUser()
+        {
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            return Ok();
+        }
+        [HttpPut("{id}")]
+        public IActionResult UpdateUser(int id)
+        {
+            return Ok();
+        }
+    }
+}
