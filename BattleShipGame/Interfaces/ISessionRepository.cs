aa632674da2321033c3f7d9f305fbed65cab55c9
Tomasz Cac <tomaszandrzej1234@gmail.com>
2026-05-18 14:57:26 +0200

    Implemented basic operations on Sessions and added in-memory data

--- BattleShipGame/Interfaces/ISessionRepository.cs
+++ BattleShipGame/Interfaces/ISessionRepository.cs
@@ -1,6 +1,12 @@
-﻿namespace BattleShipGame.Interfaces
+﻿using BattleShipGame.Models;
+
+namespace BattleShipGame.Interfaces
 {
     public interface ISessionRepository
     {
+        public Session? GetSession(int id);
+        public string CreateSession(User user);
+        public bool RemoveSession(int id);
+        public List<Session>? GetAvailableSessions();
     }
 }
--- BattleShipGame/Repositiories/SessionRepository.cs
+++ BattleShipGame/Repositiories/SessionRepository.cs
@@ -1,9 +1,42 @@
 ﻿using BattleShipGame.Interfaces;
+using BattleShipGame.Models;
+using Newtonsoft.Json;
 
 namespace BattleShipGame.Repositiories
 {
     public class SessionRepository : ISessionRepository
     {
+        private List<Session> Sessions = new();
 
+        public string CreateSession(User user)
+        {
+            Session session = new Session();
+            session.Host = user;
+            if (Sessions.Any())
+                session.Id = Sessions.Last().Id + 1;
+            else
+                session.Id = 1;
+            Sessions.Add(session);
+            return JsonConvert.SerializeObject(session);
+        }
+
+        public List<Session>? GetAvailableSessions()
+        {
+            return Sessions.FindAll(b => b.Guest == null);
+        }
+
+        public Session? GetSession(int id)
+        {
+            return Sessions.FirstOrDefault(b => b.Id == id);
+        }
+
+        public bool RemoveSession(int id)
+        {
+            var sessionToRemove = Sessions.FirstOrDefault(b => b.Id == id);
+            if (sessionToRemove != null)
+                return Sessions.Remove(sessionToRemove);
+            else
+                return false;
+        }
     }
 }
