be390a271e4630636f9193188fb3be73d7f2aae2
Tomasz Cac <tomaszandrzej1234@gmail.com>
2026-05-18 12:24:39 +0200

    Added User and Session models

---
+++ BattleShipGame/Models/Session.cs
@@ -0,0 +1,11 @@
+﻿namespace BattleShipGame.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public int[,]? BoardHost { get; set; }
+        public int[,]? BoardGuest { get; set; }
+        public User Host { get; set; }
+        public User? Guest { get; set; }
+    }
+}
---
+++ BattleShipGame/Models/User.cs
@@ -0,0 +1,11 @@
+﻿namespace BattleShipGame.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public int? Wins { get; set; }
+        public int? Losses { get; set; }
+    }
+}
