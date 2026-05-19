2ef785a2a9955a3f844b9e488c1ef70255d0f4a2
Tomasz Cac <tomaszandrzej1234@gmail.com>
2026-05-19 12:24:31 +0200

    Added encryption to User password

--- BattleShipGame/Models/User.cs
+++ BattleShipGame/Models/User.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Password { get; set; }
+        public string PasswordHash { get; set; }
         public int? Wins { get; set; }
         public int? Losses { get; set; }
     }
--- BattleShipGame/Profiles/UserProfile.cs
+++ BattleShipGame/Profiles/UserProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using BattleShipGame.Dtos;
 using BattleShipGame.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace BattleShipGame.Profiles
 {
@@ -8,7 +9,24 @@ namespace BattleShipGame.Profiles
     {
         public UserProfile()
         {
-            CreateMap<User, UserDto>();
+            CreateMap<User, UserDto>().ForMember(dest => dest.Password, opt => opt.Ignore());
+            CreateMap<UserDto, User>().ConvertUsing<UserConverter>();
+        }
+    }
+
+    public class UserConverter : ITypeConverter<UserDto, User>
+    {
+        public User Convert(UserDto source, User destination, ResolutionContext context)
+        {
+            var user = new User();
+            var passwordHasher = new PasswordHasher<User>();
+            user.Id = source.Id;
+            user.Name = source.Name;
+            user.Wins = 0;
+            user.Losses = 0;
+            user.PasswordHash = passwordHasher.HashPassword(user, source.Password);
+
+            return user;
         }
     }
 }
