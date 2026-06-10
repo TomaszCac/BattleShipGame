75b483ae6dd2ee29c82fbbe087455c6510ebe246
Tomasz Cac <tomaszandrzej1234@gmail.com>
2026-06-10 21:45:05 +0200

    Added enemy board visualisation

--- BattleShipGame_Frontend/Windows/BattleWindow.cs
+++ BattleShipGame_Frontend/Windows/BattleWindow.cs
@@ -27,6 +27,7 @@ namespace BattleShipGame_Frontend.Windows
             InitializeComponent();
             IdLabel.Text = $"Session ID: {session.Id}";
             SetupBoard();
+            SetupEnemyBoard();
 
         }
         private void SetupBoard()
@@ -72,23 +73,69 @@ namespace BattleShipGame_Frontend.Windows
                 }
             }
             changeDirectionButton = new Button();
-            changeDirectionButton.Location = new Point(600, 50);
+            changeDirectionButton.Location = new Point(625, 50);
             changeDirectionButton.Text = "Rotate";
+            changeDirectionButton.Width = 100;
+            changeDirectionButton.Height = 50;
             restartBoardButton = new Button();
-            restartBoardButton.Location = new Point(600, 100);
+            restartBoardButton.Location = new Point(625, 100);
             restartBoardButton.Text = "Restart Board";
+            restartBoardButton.Width = 100;
+            restartBoardButton.Height = 50;
             readyButton = new Button();
             readyButton.Text = "Ready";
             readyButton.Enabled = false;
-            readyButton.Location = new Point(600, 150);
+            readyButton.Location = new Point(625, 150);
+            readyButton.Width = 100;
+            readyButton.Height = 50;
             statusLabel = new Label();
-            statusLabel.Location = new Point(600, 200);
-            statusLabel.Width = 300;
+            statusLabel.Location = new Point(575, 300);
+            statusLabel.Width = 200;
             statusLabel.Height = 100;
             this.Controls.Add(statusLabel);
             this.Controls.Add(readyButton);
             this.Controls.Add(restartBoardButton);
             this.Controls.Add(changeDirectionButton);
+        }
+        public void SetupEnemyBoard()
+        {
+            for (int x = 0; x < 11; x++)
+            {
+                for (int y = 0; y < 10; y++)
+                {
+                    if (x == 0)
+                    {
+                        Label pbCordsY = new Label();
+                        pbCordsY.Width = 50;
+                        pbCordsY.Height = 50;
+                        pbCordsY.Name = $"{alphabet[y]}";
+                        pbCordsY.Text = $"{alphabet[y]}";
+                        pbCordsY.Location = new Point(800, y * 50 + 50);
+                        pbCordsY.TextAlign = ContentAlignment.MiddleCenter;
+                        Label pbCordsX = new Label();
+                        pbCordsX.Width = 50;
+                        pbCordsX.Height = 50;
+                        pbCordsX.Name = $"{y}";
+                        pbCordsX.Text = $"{y}";
+                        pbCordsX.Location = new Point(800 + y * 50 + 50, 0);
+                        pbCordsX.TextAlign = ContentAlignment.MiddleCenter;
+
+                        this.Controls.Add(pbCordsY);
+                        this.Controls.Add(pbCordsX);
+                    }
+                    else
+                    {
+                        Button btn = new Button();
+                        btn.Name = $"{x-1},{y}";
+                        btn.Text = $"{alphabet[x-1]} {y}";
+                        btn.Width = 50;
+                        btn.Height = 50;
+                        btn.Location = new Point(800 + y * 50 + 50, (x-1) * 50 + 50);
+                        this.Controls.Add(btn);
+                    }
+                    
+                }
+            }
         }
     }
 }
