80b1d224bd3bdc4e4d003fe2b816505d5412b5c9
Tomasz Cac <tomaszandrzej1234@gmail.com>
2026-06-12 14:47:36 +0200

    Hotfixed a typo

--- BattleShipGame/Api/Hubs/BattleShipHub.cs
+++ BattleShipGame/Api/Hubs/BattleShipHub.cs
@@ -7,7 +7,7 @@ namespace BattleShipGame.Api.Hubs
         public async Task PlayerConnected(string groupName, string userName)
         {
             await Groups.AddToGroupAsync(Context.ConnectionId, groupName);
-            await Clients.Group(groupName).SendAsync("Player Connected", $"{userName} has joined the game {groupName}");
+            await Clients.Group(groupName).SendAsync("PlayerConnected", $"{userName} has joined the game {groupName}");
         }
         public async Task HostGame(string groupName)
         {
