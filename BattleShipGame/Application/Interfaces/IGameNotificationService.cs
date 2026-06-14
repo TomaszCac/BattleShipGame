dd7ef56d1e16da9137e20a74ac30288af3d45d6d
Tomasz Cac <tomaszandrzej1234@gmail.com>
2026-06-14 22:27:28 +0200

    Added summaries to remaining functions

--- BattleShipGame/Application/Interfaces/IGameNotificationService.cs
+++ BattleShipGame/Application/Interfaces/IGameNotificationService.cs
@@ -2,8 +2,27 @@
 {
     public interface IGameNotificationService
     {
+        /// <summary>
+        /// Sends message to specified group if ship has been hit
+        /// </summary>
+        /// <param name="x">Int x value of x,y board</param>
+        /// <param name="y">Int y value of x,y board</param>
+        /// <param name="sessionId">Id value of a specific session</param>
+        /// <param name="isHit">Bool value if ship has been hit</param>
+        /// <returns></returns>
         public Task ShipHit(int x, int y, int sessionId, bool isHit);
+        /// <summary>
+        /// Sends message to specified group if player has won the game
+        /// </summary>
+        /// <param name="sessionId">Id value of a specific session</param>
+        /// <param name="turn">Bool value of which user has won true if host false if guest</param>
+        /// <returns></returns>
         public Task WinGame(int sessionId, bool turn);
+        /// <summary>
+        /// Sends message to specified group about started fight
+        /// </summary>
+        /// <param name="sessionId">Id value of a specific session</param>
+        /// <returns></returns>
         public Task StartFight(int sessionId);
 
     }
--- BattleShipGame/Infrastructure/SignalR/SignalRGameNotificationService.cs
+++ BattleShipGame/Infrastructure/SignalR/SignalRGameNotificationService.cs
@@ -13,14 +13,33 @@ namespace BattleShipGame.Infrastructure.SignalR
             _hubContext = hubContext;
         }
 
+        /// <summary>
+        /// Sends message to specified group if ship has been hit
+        /// </summary>
+        /// <param name="x">Int x value of x,y board</param>
+        /// <param name="y">Int y value of x,y board</param>
+        /// <param name="sessionId">Id value of a specific session</param>
+        /// <param name="isHit">Bool value if ship has been hit</param>
+        /// <returns></returns>
         public async Task ShipHit(int x, int y, int sessionId, bool isHit)
         {
             await _hubContext.Clients.Group(sessionId.ToString()).SendAsync("ShipHit", x, y, isHit);
         }
+        /// <summary>
+        /// Sends message to specified group if player has won the game
+        /// </summary>
+        /// <param name="sessionId">Id value of a specific session</param>
+        /// <param name="turn">Bool value of which user has won true if host false if guest</param>
+        /// <returns></returns>
         public async Task WinGame(int sessionId, bool turn)
         {
             await _hubContext.Clients.Group(sessionId.ToString()).SendAsync("WinGame", turn);
         }
+        /// <summary>
+        /// Sends message to specified group about started fight
+        /// </summary>
+        /// <param name="sessionId">Id value of a specific session</param>
+        /// <returns></returns>
         public async Task StartFight(int sessionId)
         {
             await _hubContext.Clients.Group(sessionId.ToString()).SendAsync("StartFight");
